1b07bfaae2be9e8a4c325f091f9e43d852d59005
Haaxor1689 <betkomaros@gmail.com>
2022-02-10 23:56:05 +0100

    Fix inventories in generated save files

--- src/utils/data/blankSaveFile.ts
+++ src/utils/data/blankSaveFile.ts
@@ -39,9 +39,9 @@ const BlankSaveFile = (
 	player_bomb_level: 0,
 	player_stats: [50, 3, 0, 0, 0, 0, 0, 0, 0, 0],
 	player_2_stats: [50, 3, 0, 0, 0, 0, 0, 0, 0, 0],
-	inventory: [...Array(32).keys()].map(() => 'FFFF000').join(),
-	storage: [...Array(104).keys()].map(() => 'FFFF000').join(),
-	equipment: [...Array(6).keys()].map(() => 'FFFF000').join(),
+	inventory: [...Array(32).keys()].map(() => 'FFFF000').join(''),
+	storage: [...Array(104).keys()].map(() => 'FFFF000').join(''),
+	equipment: [...Array(6).keys()].map(() => 'FFFF000').join(''),
 	inventory_materials:
 		'000100000100020000010003000001000700000100050000010006000001000A0000010008000001000C000001000D00000100100000010011000001000F00000100120000010015000001001600000102580000010259000001025A000001025C000001025B000001025D000001025E000001025F0000010260000001026100000103840000010388000000038C000000001A000001FFFF000001FFFF000001',
 	badges: '0000000000000000000000000000000000000000',
